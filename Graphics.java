0ce5eab86ac5c7a8a843c8501331808a9461f2ff
Hallie Stringham <126368145+Heensle@users.noreply.github.com>
2025-06-06 09:16:18 -0400

    Create Graphics.java

---
+++ Graphics.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Graphics {
+    int score;
+    int lives;
+    ArrayList<Character> fruits;
+    
+    public Graphics(){
+        score = 0;
+        lives = 2;
+        ArrayList<Character> fruits = new ArrayList<Character>();
+    }
+}
