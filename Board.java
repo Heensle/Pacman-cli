2b4fca8473b040915914a1aacac78bf9fbc3b3f4
Hallie Stringham <126368145+Heensle@users.noreply.github.com>
2025-06-06 09:17:50 -0400

    Added checks for movement & eat pellet function

--- Board.java
+++ Board.java
@@ -4,6 +4,7 @@ public class Board {
     
     
     public Board(){
+        //creates board set for level 1
         
         map = new char[31][55];
         
@@ -192,6 +193,43 @@ public class Board {
         for(int i = 0; i < 55; i++){
             map[30][i] = row.charAt(i);
         }
+    }
+    
+    public boolean can_move(Pacman pellet_eater){
+        int PMxpos = pellet_eater.get_xpos();
+        int PMypos = pellet_eater.get_ypos();
+        if(pellet_eater.get_dir() == 10){
+            PMypos--;
+        } else if (pellet_eater.get_dir() == 11){
+            PMypos++;
+        } else if (pellet_eater.get_dir() == 00){
+            PMxpos++;
+        } else if (pellet_eater.get_dir() == 01){
+            PMxpos--;
+        }
+        if(PMxpos < 0 || PMxpos > 54){
+            return true;
+        }
+        if(map[PMypos][PMxpos] != ' ' && map[PMypos][PMxpos] != '●' && map[PMypos][PMxpos] != '◍'){
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public boolean eat_dot(Pacman pellet_eater){
+        //returns true if power pellet
+        int PMxpos = pellet_eater.get_xpos();
+        int PMypos = pellet_eater.get_ypos();
+        
+        boolean isPowerPellet = false;;
+        if(map[PMypos][PMxpos] == '◍'){
+            isPowerPellet = true;
+        }
+        
+        map[PMypos][PMxpos] = ' ';
+        
+        return isPowerPellet;
     }
     
     public void print_board(){
