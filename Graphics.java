8ddcb11b3db2786238f57fcf14a021913cf92ce9
Hallie Stringham <126368145+Heensle@users.noreply.github.com>
2025-06-06 21:49:05 -0400

    Graphics setters and getters

--- Graphics.java
+++ Graphics.java
@@ -10,4 +10,28 @@ public class Graphics {
         lives = 2;
         ArrayList<Character> fruits = new ArrayList<Character>();
     }
+    
+    public void set_score(int score){
+        this.score = score;
+    }
+    
+    public void set_lives(int lives){
+        this.lives = lives;
+    }
+    
+    public void add_fruit(char fruit){
+        fruits.add(fruit);
+    }
+    
+    public int get_score(){
+        return score;
+    }
+    
+    public int get_lives(){
+        return lives;
+    }
+    
+    public char get_fruit(int index){
+        return fruits.get(index);
+    }
 }
