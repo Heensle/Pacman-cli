9329dddf3f32d56cf94e77800b8024195ed6f754
Hallie Stringham <126368145+Heensle@users.noreply.github.com>
2025-06-09 18:59:53 -0400

    Update Pacman.java
    Added constructor, eat_dot(), and win() functions.

--- Pacman.java
+++ Pacman.java
@@ -1,3 +1,33 @@
 public class Pacman extends Entity {
+    int pellets_eaten;
     
+    public Pacman(){
+        set_dir(10);
+        set_xpos(27);
+        set_ypos(23);
+    }
+    
+    public boolean eat_dot(Board board){
+        //returns true if power pellet
+        
+        boolean isPowerPellet = false;
+        if(board.board_at(get_xpos(), get_ypos()) == '◍'){
+            isPowerPellet = true;
+            pellets_eaten++;
+        } else if (board.board_at(get_xpos(), get_ypos()) == '●'){
+            pellets_eaten++;
+        }
+        
+        board.edit_board(get_xpos(), get_ypos(), ' ');
+        
+        return isPowerPellet;
+    }
+    
+    public boolean win(){
+        if(pellets_eaten == 244){
+            System.out.println("\033[32;1HYOU WIN!");
+            return true;
+        }
+        return false;
+    }
 }
