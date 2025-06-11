2a30bb39f3d461775d797b85d2efb1771cec2eff
Hallie Stringham <126368145+Heensle@users.noreply.github.com>
2025-06-11 10:19:35 -0400

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -1,24 +1,74 @@
 public class Entity {
-    private int xpos;
-    private int ypos;
-    private int dir; //10-up, 11-down, 00-right, 01-left
+    private Vector vector;
+    private char appearance;
+    private String color;
+    
+    public Entity(int xpos, int ypos, int dir, char looks, String color){
+        vector = new Vector(xpos, ypos, dir);
+        appearance = looks;
+        this.color = color;
+    }
+    
+    public void update_pos(){
+        if(vector.get_dir() == 10){
+            vector.set_ypos(vector.get_ypos() - 1);
+        } else if (vector.get_dir() == 11){
+            vector.set_ypos(vector.get_ypos() + 1);
+        } else if (vector.get_dir() == 00){
+            vector.set_xpos(vector.get_xpos() + 1);
+        } else if (vector.get_dir() == 01){
+            vector.set_xpos(vector.get_xpos() - 1);
+        }
+        
+        if(vector.get_xpos() < 0){
+            vector.set_xpos(53);
+        }
+        if(vector.get_xpos() > 53){
+            vector.set_xpos(0);
+        }
+    }
+    
+    public double distance(OrderedPair destination){
+        return vector.distance(destination);
+    }
     
     public void set_xpos(int xpos){
-        this.xpos = xpos;
+        vector.set_xpos(xpos);
     }
     public void set_ypos(int ypos){
-        this.ypos = ypos;
+        vector.set_ypos(ypos);
     }
     public void set_dir(int dir){
-        this.dir = dir;
+        vector.set_dir(dir);
+    }
+    public void set_vector(int xpos, int ypos, int dir){
+        vector.set_xpos(xpos);
+        vector.set_ypos(ypos);
+        vector.set_dir(dir);
+    }
+    public void set_appearance(char looks){
+        appearance = looks;
+    }
+    public void set_color(String color){
+        this.color = color;
     }
     public int get_xpos(){
-        return this.xpos;
+        return vector.get_xpos();
     }
     public int get_ypos(){
-        return this.ypos;
+        return vector.get_ypos();
     }
     public int get_dir(){
-        return dir;
+        return vector.get_dir();
+    }
+    
+    public Vector get_vector(){
+        return vector;
+    }
+    public char get_appearance(){
+        return appearance;
+    }
+    public String get_color(){
+        return color;
     }
 }
